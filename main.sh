b0ad3d79b915552eb78652107fd01240c5ed41b1
Golang Uzbekistan <golanguzb70@gmail.com>
2023-06-10 17:03:17 +0500

    add manager first version

--- main.sh
+++ main.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+echo "Hello, good luck with your new project."
+echo "Choose a template to start."
+echo '1. Golang, postgres, gin, swagger.'
+
+
+read template;
+
+if [ $template -eq 1 ]; then
+    $TEMPLATE_PATH/'go-gin-basicauth-postgres-monolithic.sh'
+fi
