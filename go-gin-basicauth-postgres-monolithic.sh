e400edb4f016ae8ea5a337bd0589f0c5355c3ea8
Golang Uzbekistan <golanguzb70@gmail.com>
2023-06-11 18:53:51 +0500

    add copyright while generating a template

--- go-gin-basicauth-postgres-monolithic.sh
+++ go-gin-basicauth-postgres-monolithic.sh
@@ -77,8 +77,6 @@ process_files_to_edit() {
                 process_files_to_edit "$file"  # Recursively process subfolders
             fi
         else
-            # find  ./* -type f -exec sed -i "s|template|$crudname|g" {} +
-            # find  ./* -type f -exec sed -i "s|Template|$title_crud|g" {} +
             update_word "$file"  
         fi
     done
@@ -94,6 +92,43 @@ data2="export GO_MOD_URL='$go_module'"
 # Write the new data to the file
 echo "$data1" > "$file_path"
 echo "$data2" >> "$file_path"
+
+
+# Update README.md file.
+
+# Define the lines of text to be added
+lines=(
+"# Copyright"
+"This code template is originally designed and written by [Azizbek Hojimurotov](https://github.com/golanguzb70) and other [contributers](https://github.com/golanguzb70/go-gin-basicauth-postgres-monolithic-template/graphs/contributors)."
+"Please don't delete or edit this part and [license](https://github.com/golanguzb70/go-gin-basicauth-postgres-monolithic-template/blob/main/LICENSE)."
+""
+)
+
+# Specify the file path
+file_path="$TEMPLATE_PATH/templates/go-gin-basicauth-postgres-monolithic-template/README.md"
+
+# Create a temporary file
+tmp_file=$(mktemp)
+
+# Loop through the lines of text in reverse order and add them to the temporary file
+for ((i=0; i<${#lines[@]}; i++)); do
+    echo "${lines[i]}" >> "$tmp_file"
+done
+
+# Append the existing contents of the file to the temporary file
+cat "$file_path" >> "$tmp_file"
+cat "$tmp_file" > "$file_path" 
+
+
+# Overwrite the original file with the contents of the temporary file
+# mv "$tmp_file" "$file_path"
+
+
+# Cleanup the temporary file
+rm "$tmp_file"
+
+echo "New lines of text have been added to the beginning of the file."
+
 
 # Copy the code to your current directory.
 cp -r $TEMPLATE_PATH/templates/go-gin-basicauth-postgres-monolithic-template/* $current_path
